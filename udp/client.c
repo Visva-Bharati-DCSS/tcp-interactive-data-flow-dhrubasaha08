ebd15343edc7ff6540a45d382c4e777b0f38bf4b
dhrubasaha08 <dhrubasaha@outlook.com>
2023-05-16 11:37:04 -0700

    origin

--- udp/client.c
+++ udp/client.c
@@ -1,62 +1,113 @@
-#include <arpa/inet.h>
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <arpa/inet.h>
+#include <errno.h>
 #include <time.h>
+#include <sys/time.h>
 
-#define MAXBUFFSZ 320
 #define SA struct sockaddr
 
-typedef struct {
+typedef struct
+{
     unsigned int numElmt;
-    double val[25];
-} myMsg_t;
+    union mydata
+    {
+        float dElmt;
+        unsigned int iElmt;
+    } a[10];
+} MyMsg_t;
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Usage: ./client <IP address> <port>\n");
-        exit(1);
+int sockfd, to_exit = 0, i, select_no;
+socklen_t servAddrLen;
+struct sockaddr_in cliAddr, servAddr;
+
+void ConvertToNbw(MyMsg_t *msg, int num)
+{
+    int i, j;
+    unsigned int tmp;
+    msg->numElmt = htonl(num);
+    for (i = 0; i < num; i++)
+    {
+        tmp = htonl(msg->a[i].iElmt);
+        msg->a[i].iElmt = tmp;
     }
+}
 
-    int sockfd;
-    struct sockaddr_in servAddr;
-    myMsg_t txMsg;
-    char rxMsg[MAXBUFFSZ];
+int main(int argc, char **argv)
+{
+    MyMsg_t txMsg;
+    unsigned char txbuff[80];
+    char rxMsg[10];
+    int i, j, select_no, servAddrLen;
+    fd_set readfd;
+
+    if (argc < 3)
+    {
+        printf("Usage : <myclient> <server IP address> <server port>\n");
+        exit(0);
+    }
 
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    memset(&servAddr, 0, sizeof(servAddr));
+    if (sockfd < 0)
+    {
+        switch (errno)
+        {
+        case EACCES:
+            printf("Permission to create socket of the specified type is denied\n");
+            break;
+
+        case EAFNOSUPPORT:
+            printf("Socket of given address family not supported\n");
+            break;
+
+        case EINVAL:
+            printf("Invalid values in type\n");
+            break;
+
+        default:
+            printf("Other socket errors\n");
+            break;
+        }
+        exit(0);
+    }
+
+    bzero(&servAddr, sizeof(servAddr));
     servAddr.sin_family = AF_INET;
-    servAddr.sin_addr.s_addr = inet_addr(argv[1]);
+    inet_pton(AF_INET, argv[1], &servAddr.sin_addr);
+    printf("%x\n", ntohl(servAddr.sin_addr.s_addr));
     servAddr.sin_port = htons(atoi(argv[2]));
 
-    txMsg.numElmt = 25;
-    srand(time(0));
+    srand48(17003);
 
-    for (unsigned int i = 0; i < txMsg.numElmt; i++) {
-        txMsg.val[i] = (double)(rand() % 1000) / 100;
+    for (;;)
+    {
+        for (i = 0; i < 10; i++)
+        {
+            txMsg.a[i].dElmt = (float)(-10.0 + 20.0 * drand48());
+            printf("%e ", txMsg.a[i].dElmt);
+        }
+        printf("\n");
+
+        ConvertToNbw(&txMsg, 10);
+
+        sendto(sockfd, &txMsg, sizeof(txMsg), 0,
+               (struct sockaddr *)&servAddr, sizeof(servAddr));
+
+        bzero(rxMsg, 10);
+        recvfrom(sockfd, &rxMsg, 10, 0, (struct sockaddr *)&servAddr, &servAddrLen);
+        fputs(rxMsg, stdout);
+        if (!strncmp(rxMsg, "NEXT MSG", 10))
+        {
+            printf("Sever->client : ");
+            fputs(rxMsg, stdout);
+            printf("\n");
+        }
+        else
+        {
+            printf("Terminated from server\n");
+            break;
+        }
     }
-
-    socklen_t servAddrLen = sizeof(servAddr);
-    if (sendto(sockfd, &txMsg, sizeof(txMsg), 0, (SA *)&servAddr, servAddrLen) < 0) {
-        perror("sendto error");
-        exit(1);
-    }
-
-    // Receive value v from the server
-    if (recvfrom(sockfd, rxMsg, sizeof(rxMsg), 0, (SA *)&servAddr, &servAddrLen) < 0) {
-        perror("recvfrom error");
-        exit(1);
-    }
-
-    double v;
-    sscanf(rxMsg, "%lf", &v);
-    printf("Received value v: %f\n", v);
-
-    close(sockfd);
-
-    return 0;
 }
--- udp/server.c
+++ udp/server.c
@@ -1,129 +1,93 @@
-#include <arpa/inet.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <netinet/in.h>
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <time.h>
 #include <unistd.h>
 
-#define SERV_PORT 9877
-#define MAXBUFFSZ 320
-#define LISTENQ 1024
-#define MAX_CLIENTS 256
-#define MAX_VALUES 5
 #define SA struct sockaddr
+#define MAX_CLIENTS 256
 
-typedef struct {
-    unsigned int numElmt;
-    double val[25];
-} myMsg_t;
+typedef struct
+{
+  unsigned int numElmt;
+  union mydata
+  {
+    float dElmt;
+    unsigned int iElmt;
+  } a[10];
+} MyMsg_t;
 
-typedef struct {
-    unsigned char id;
-    double values[MAX_VALUES];
-    int value_count;
-    double sum;
-} client_data_t;
+void ConvertToHostByteOrder(MyMsg_t *msg)
+{
+  int i, j;
+  unsigned char rb[80];
 
-client_data_t clients[MAX_CLIENTS];
+  msg->numElmt = ntohl(msg->numElmt);
 
-void init_clients() {
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        clients[i].id = i;
-        clients[i].value_count = 0;
-        clients[i].sum = 0.0;
-        for (int j = 0; j < MAX_VALUES; j++) {
-            clients[i].values[j] = 0.0;
-        }
+  for (i = 0; i < (msg->numElmt); i++)
+  {
+    msg->a[i].iElmt = ntohl(msg->a[i].iElmt);
+  }
+
+  for (i = 0; i < msg->numElmt; i++)
+    printf("%e ", msg->a[i].dElmt);
+  printf("\n");
+}
+
+int main(int argc, char *argv[])
+{
+  MyMsg_t rxMsg;
+  char *txMsg = "NEXT MSG";
+  int sockfd, numRxMsg;
+  unsigned short cliPort;
+  struct sockaddr_in servAddr, cliAddr;
+  char cli_ip_addr[16];
+  socklen_t cliAddrLen;
+
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  bzero(&servAddr, sizeof(servAddr));
+  servAddr.sin_family = AF_INET;
+  servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servAddr.sin_port = htons(atoi(argv[1]));
+  numRxMsg = atoi(argv[2]);
+  if (bind(sockfd, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
+  {
+    if (errno == EADDRINUSE)
+    {
+      printf("Bind error ... Port is still in use\n");
     }
-}
-
-unsigned char generate_client_id(struct sockaddr_in *client_addr) {
-    unsigned int ip = ntohl(client_addr->sin_addr.s_addr);
-    unsigned int port = ntohs(client_addr->sin_port);
-    return (unsigned char)((ip + port) % MAX_CLIENTS);
-}
-
-void update_client_data(unsigned char id, double value, double threshold) {
-    client_data_t *client = &clients[id];
-    client->sum -= client->values[client->value_count % MAX_VALUES];
-    client->values[client->value_count % MAX_VALUES] = value;
-    client->sum += value;
-    client->value_count++;
-
-    double average = client->sum / (client->value_count < MAX_VALUES ? client->value_count : MAX_VALUES);
-
-    if (average > threshold) {
-        printf("Archiving: Client ID %u - Average value: %f\n", id, average);
+    else
+    {
+      printf("Bind error ... \n");
     }
-}
-
-void sigint_handler(int signal) {
-    printf("\nCaught signal %d. Shutting down server...\n", signal);
     exit(0);
-}
+  }
+  cliAddrLen = sizeof(cliAddr);
+  bzero(cli_ip_addr, 16);
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Usage: ./server <port> <v>\n");
-        exit(1);
+  while (recvfrom(sockfd, &rxMsg, sizeof(rxMsg), 0, (struct sockaddr *)&cliAddr, &cliAddrLen))
+  {
+    inet_ntop(AF_INET, &(cliAddr.sin_addr), &cli_ip_addr[0], 16);
+    printf("Client IP address : ");
+    puts(cli_ip_addr);
+    cliPort = ntohs(cliAddr.sin_port);
+    printf("Client port id : %u\n", cliPort);
+    ConvertToHostByteOrder(&rxMsg);
+    numRxMsg--;
+    if (numRxMsg == 0)
+    {
+      sendto(sockfd, "STOP", 5, 0, (struct sockaddr *)&cliAddr, cliAddrLen);
+      break;
     }
-
-    int port = atoi(argv[1]);
-    double v = atof(argv[2]);
-
-    int sockfd;
-    myMsg_t rxMsg;
-    char txMsg[MAXBUFFSZ];
-    struct sockaddr_in servAddr, cliAddr;
-    socklen_t cliAddrLen;
-
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    memset(&servAddr, 0, sizeof(servAddr));
-    servAddr.sin_family = AF_INET;
-    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servAddr.sin_port = htons(port);
-
-    if (bind(sockfd, (struct sockaddr *)&servAddr, sizeof(servAddr)) < 0) {
-        perror("bind error");
-        exit(1);
+    else
+    {
+      sleep(1);
+      printf("Asking for next message\n");
+      sendto(sockfd, txMsg, sizeof(txMsg), 0, (struct sockaddr *)&cliAddr, cliAddrLen);
     }
-
-    signal(SIGINT, sigint_handler);
-
-    init_clients();
-
-    for (;;) {
-        cliAddrLen = sizeof(cliAddr);
-        memset(&rxMsg, 0, sizeof(rxMsg));
-
-        int recv_len = recvfrom(sockfd, &rxMsg, sizeof(rxMsg), 0, (SA *)&cliAddr, &cliAddrLen);
-        if (recv_len < 0) {
-            perror("recvfrom error");
-            exit(1);
-}
-    // Generate client ID based on the client's IP and port
-    unsigned char client_id = generate_client_id(&cliAddr);
-
-    // Send value v to the client
-    snprintf(txMsg, sizeof(txMsg), "%.16lf", v);
-    if (sendto(sockfd, txMsg, strlen(txMsg), 0, (SA *)&cliAddr, cliAddrLen) < 0) {
-        perror("sendto error");
-        exit(1);
-    }
-
-    // Update client data and check the average
-    for (unsigned int i = 0; i < rxMsg.numElmt; i++) {
-        update_client_data(client_id, rxMsg.val[i], 0.75 * v);
-    }
-}
-
-close(sockfd);
-
-return 0;
+  }
+  printf("%x\n", cliAddr.sin_addr.s_addr);
 }
